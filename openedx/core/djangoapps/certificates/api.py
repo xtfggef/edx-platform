de3ad32519430a52c0988004a475364739b2ffde
Alex Dusenbery <adusenbery@edx.org>
2017-09-11 11:32:07 -0400

    checkpoint

--- openedx/core/djangoapps/certificates/api.py
+++ openedx/core/djangoapps/certificates/api.py
@@ -37,3 +37,7 @@ def _enabled_and_self_paced(course):
 
 def can_show_certificate_available_date_field(course):
     return _enabled_and_self_paced(course)
+
+
+def can_show_view_certificate_button(enrollment):
+    return
--- openedx/core/djangoapps/certificates/tests/test_api.py
+++ openedx/core/djangoapps/certificates/tests/test_api.py
@@ -5,9 +5,12 @@ from unittest import TestCase
 import ddt
 import waffle
 
+from lms.djangoapps.certificates.tests.factories import GeneratedCertificateFactory
+from lms.djangoapps.certificates import CertificateStatuses, GeneratedCertificate
 from openedx.core.djangoapps.certificates import api
 from openedx.core.djangoapps.certificates.config import waffle as certs_waffle
 from openedx.core.djangoapps.content.course_overviews.tests.factories import CourseOverviewFactory
+from student.tests.factories import CourseEnrollmentFactory, UserFactory
 
 
 @contextmanager
@@ -19,16 +22,18 @@ def configure_waffle_namespace(self_paced_enabled, instructor_paced_enabled):
             yield
 
 
-@ddt.ddt
-class FeatureEnabledTestCase(TestCase):
+class CertificatesApiBaseTestCase(TestCase):
     def setUp(self):
-        super(FeatureEnabledTestCase, self).setUp()
+        super(CertificatesApiBaseTestCase, self).setUp()
         self.course = CourseOverviewFactory.create()
 
     def tearDown(self):
-        super(FeatureEnabledTestCase, self).tearDown()
+        super(CertificatesApiBaseTestCase, self).tearDown()
         self.course.self_paced = False
 
+
+@ddt.ddt
+class FeatureEnabledTestCase(CertificatesApiBaseTestCase):
     @ddt.data(*itertools.product((True, False), (True, False)))
     @ddt.unpack
     def test_auto_certificate_generation_enabled(self, self_paced_enabled, instructor_paced_enabled):
@@ -51,6 +56,19 @@ class FeatureEnabledTestCase(TestCase):
         with configure_waffle_namespace(self_paced_enabled, instructor_paced_enabled):
             self.assertEqual(expected_value, api.auto_certificate_generation_enabled_for_course(self.course))
 
+
+@ddt.ddt
+class VisibilityTestCase(CertificatesApiBaseTestCase):
+    def setUp(self):
+        super(VisibilityTestCase, self).setUp()
+        self.user = UserFactory.create()
+        self.enrollment = CourseEnrollmentFactory(
+            user=self.user,
+            course_id=self.course.id,
+            is_active=True,
+            mode='audit',
+        )
+
     @ddt.data(
         (True, False, True, False),  # feature enabled and self-paced should return False
         (False, True, False, True),  # feature enabled and instructor-paced should return True
@@ -64,3 +82,23 @@ class FeatureEnabledTestCase(TestCase):
         self.course.self_paced = is_self_paced
         with configure_waffle_namespace(self_paced_enabled, instructor_paced_enabled):
             self.assertEqual(expected_value, api.can_show_certificate_available_date_field(self.course))
+
+    @ddt.data(
+        ('honor', CertificateStatuses.downloadable, ),
+        ('verified', True, True),
+        ('honor', False, False),
+        ('verified', False, False),
+        (None, True, False), # check that any other enrollment mode won't show button
+    )
+    @ddt.unpack
+    def test_can_show_view_certificate_button(
+            self, enrollment_mode, certificate_status, certificate_mode, expected_value
+    ):
+        self.enrollment.mode = enrollment_mode
+        certificate = GeneratedCertificateFactory(
+            user=self.user,
+            course_id=self.course.id,
+            grade='1.0',
+            status=certificate_status,
+            mode=certificate_mode,
+        )
