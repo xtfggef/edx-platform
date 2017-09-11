bb43206faed5e2f651cf88c909b2cba42d5294c1
Mushtaq Ali <mushtaque@edx.org>
2017-09-11 19:04:47 +0500

    Add video transcript config model flags - EDUCATOR-1224

--- openedx/core/djangoapps/video_config/admin.py
+++ openedx/core/djangoapps/video_config/admin.py
@@ -5,8 +5,13 @@ Django admin dashboard configuration for Video XModule.
 from config_models.admin import ConfigurationModelAdmin, KeyedConfigurationModelAdmin
 from django.contrib import admin
 
-from openedx.core.djangoapps.video_config.forms import CourseHLSPlaybackFlagAdminForm
-from openedx.core.djangoapps.video_config.models import CourseHLSPlaybackEnabledFlag, HLSPlaybackEnabledFlag
+from openedx.core.djangoapps.video_config.forms import (
+    CourseHLSPlaybackFlagAdminForm, CourseVideoTranscriptFlagAdminForm
+)
+from openedx.core.djangoapps.video_config.models import (
+    CourseHLSPlaybackEnabledFlag, HLSPlaybackEnabledFlag,
+    CourseVideoTranscriptEnabledFlag, VideoTranscriptEnabledFlag
+)
 
 
 class CourseHLSPlaybackEnabledFlagAdmin(KeyedConfigurationModelAdmin):
@@ -23,5 +28,22 @@ class CourseHLSPlaybackEnabledFlagAdmin(KeyedConfigurationModelAdmin):
         }),
     )
 
+
+class CourseVideoTranscriptEnabledFlagAdmin(KeyedConfigurationModelAdmin):
+    """
+    Admin of Video Transcript feature on course-by-course basis.
+    Allows searching by course id.
+    """
+    form = CourseHLSPlaybackFlagAdminForm
+    search_fields = ['course_id']
+    fieldsets = (
+        (None, {
+            'fields': ('course_id', 'enabled'),
+            'description': 'Enter a valid course id. If it is invalid, an error message will be displayed.'
+        }),
+    )
+
 admin.site.register(HLSPlaybackEnabledFlag, ConfigurationModelAdmin)
 admin.site.register(CourseHLSPlaybackEnabledFlag, CourseHLSPlaybackEnabledFlagAdmin)
+admin.site.register(VideoTranscriptEnabledFlag, ConfigurationModelAdmin)
+admin.site.register(CourseVideoTranscriptEnabledFlag, CourseHLSPlaybackEnabledFlagAdmin)
--- openedx/core/djangoapps/video_config/forms.py
+++ openedx/core/djangoapps/video_config/forms.py
@@ -7,7 +7,7 @@ from django import forms
 from opaque_keys import InvalidKeyError
 from opaque_keys.edx.locator import CourseLocator
 
-from openedx.core.djangoapps.video_config.models import CourseHLSPlaybackEnabledFlag
+from openedx.core.djangoapps.video_config.models import CourseHLSPlaybackEnabledFlag, CourseVideoTranscriptEnabledFlag
 from xmodule.modulestore.django import modulestore
 
 log = logging.getLogger(__name__)
@@ -20,6 +20,68 @@ class CourseHLSPlaybackFlagAdminForm(forms.ModelForm):
 
     class Meta(object):
         model = CourseHLSPlaybackEnabledFlag
+        fields = '__all__'
+
+    def clean_course_id(self):
+        """
+        Validate the course id
+        """
+        cleaned_id = self.cleaned_data["course_id"]
+        try:
+            course_key = CourseLocator.from_string(cleaned_id)
+        except InvalidKeyError:
+            msg = u'Course id invalid. Entered course id was: "{course_id}."'.format(
+                course_id=cleaned_id
+            )
+            raise forms.ValidationError(msg)
+
+        if not modulestore().has_course(course_key):
+            msg = u'Course not found. Entered course id was: "{course_key}". '.format(
+                course_key=unicode(course_key)
+            )
+            raise forms.ValidationError(msg)
+
+        return course_key
+
+
+class CourseSpecificFlagAdminForm(forms.ModelForm):
+    """
+    Form for course-specific feature configuration.
+    """
+
+    class Meta(object):
+        abstract = True
+        fields = '__all__'
+
+    def clean_course_id(self):
+        """
+        Validate the course id
+        """
+        cleaned_id = self.cleaned_data["course_id"]
+        try:
+            course_key = CourseLocator.from_string(cleaned_id)
+        except InvalidKeyError:
+            msg = u'Course id invalid. Entered course id was: "{course_id}."'.format(
+                course_id=cleaned_id
+            )
+            raise forms.ValidationError(msg)
+
+        if not modulestore().has_course(course_key):
+            msg = u'Course not found. Entered course id was: "{course_key}". '.format(
+                course_key=unicode(course_key)
+            )
+            raise forms.ValidationError(msg)
+
+        return course_key
+
+
+class CourseVideoTranscriptFlagAdminForm(forms.ModelForm):
+    """
+    Form for course-specific Video Transcript configuration.
+    """
+
+    class Meta(object):
+        model = CourseVideoTranscriptEnabledFlag
         fields = '__all__'
 
     def clean_course_id(self):
